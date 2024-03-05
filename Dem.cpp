caaa5829ebc7636f86751277403f14f010110411
MAY 08 <MAY 08@LAB2-15>
2024-03-05 13:34:09 +0700

    Add project files.

---
+++ Dem.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string userInput;
+    cout << "Nhap mot cau bat ky: ";
+    getline(cin, userInput);
+
+    int wordCount = 0, charCount = 0;
+    for (char c : userInput) {
+        if (c == ' ') {
+            wordCount++;
+        }
+        else {
+            charCount++;
+        }
+    }
+    wordCount++; // Đếm từ cuối cùng
+    cout << "So tu trong cau la : " << wordCount << endl;
+    cout << "So ky tu trong cau la : " << charCount << endl;
+
+    return 0;
+}
